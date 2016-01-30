09abeab2144cf11cb29326ea2c3e98a0722647c1
Aaron <aschryver-cc@conestogac.on.ca>
2016-01-29 23:43:36 -0500

    Created qbXmlExport class... forgot to add link to formatting info

--- young-haze-7492/young-haze-7492/Helpers/qbXmlExport.cs
+++ young-haze-7492/young-haze-7492/Helpers/qbXmlExport.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+/*
+ * Link to qbXml formatting information:
+ * http://wiki.consolibyte.com/wiki/doku.php/quickbooks_example_qbxml
+ */
+
 namespace young_haze_7492.Helpers
 {
     public class qbXmlExport
