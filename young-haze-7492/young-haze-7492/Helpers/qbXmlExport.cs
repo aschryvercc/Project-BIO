807ce63adbe9641216b76549dca3e1e25bd8e763
Aaron <aschryver-cc@conestogac.on.ca>
2016-02-03 13:56:57 -0500

    Added QB SDK Reference

--- young-haze-7492/young-haze-7492/Helpers/qbXmlExport.cs
+++ young-haze-7492/young-haze-7492/Helpers/qbXmlExport.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using QBFC13Lib;
 
 /*
  * Link to qbXml formatting information:
  * http://wiki.consolibyte.com/wiki/doku.php/quickbooks_example_qbxml
+ * For an alternative SDK api use:
+ * http://stackoverflow.com/questions/16458778/connecting-to-quickbooks-using-api
  */
 
 namespace young_haze_7492.Helpers
 {
     public class qbXmlExport
     {
+        
     }
 }
