b6e0c3eb6b6993530c27a1b0504499a460848c94
Aaron <aschryver-cc@conestogac.on.ca>
2016-02-11 17:17:12 -0500

    'Merge' qbExportService code from 'Rv1' branch

---
+++ qbExportService/qbExportService/IqbExportService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace qbExportService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IqbExportService" in both code and config file together.
+    [ServiceContract]
+    public interface IqbExportService
+    {
+        [OperationContract]
+        string[] authenticate(string userName, string password);
+
+        [OperationContract]
+        string clientVersion(string version);
+
+        [OperationContract]
+        string closeConnection(string ticket);
+
+        [OperationContract]
+        string connectionError(string ticket, string result, string msg);
+
+        [OperationContract]
+        string getLastError(string ticket);
+
+        [OperationContract]
+        int receiveResponseXML(string ticket, string response, string result, string msg);
+
+        [OperationContract]
+        string sendRequestXML(string ticket, string response, string companyFileName,
+                              string qbXMLCountry, int qbXMLMajorVersion, int qbXMLMinorVersion);
+    }
+}
