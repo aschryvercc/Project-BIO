d58e7e53793b4fd4e2f66b991dc4ec8ab68b6f13
Aaron <aschryver-cc@conestogac.on.ca>
2016-03-14 23:55:49 -0400

    Added the dbconnector to the scheduler; Not finished though

--- DbScheduler/DbScheduler/RepeatableJob.cs
+++ DbScheduler/DbScheduler/RepeatableJob.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data;
+using DbConnector;
+
 namespace DbScheduler
 {
     class RepeatableJob : Job
     {
         private int counter = 0;
-        //private DbConnector dbConnector() = new DbConnector();
+        private DbConnectorInfo sourceInfo;
+        private DbConnectorInfo destinationInfo;
 
         public override string GetName()
         {
@@ -19,21 +23,25 @@ namespace DbScheduler
         public override void DoJob()
         {
             /*
-             * connect to biolinks
-             * 
-             * connect to biotrack
-             * 
-             * find new data in tables
-             * 
-             * pull data from tables
-             * 
-             * verify new data
-             * 
-             * push new data to database
-             * 
-             * disconnect from both databases
-             * 
+             * TODO: Properly read in connection information here.
              */
+            sourceInfo = new DbConnectorInfo();
+            destinationInfo = new DbConnectorInfo();
+
+            DbConnection sourceConnection = new DbConnection(sourceInfo);
+            DbConnection destinationConnection = new DbConnection(destinationInfo);
+
+            /*
+             * Pull data from BioLinks.
+             * TODO: Figure out wtf this method even takes.
+             */
+            DataTable sourceData sourceConnection.PullData();
+
+            /*
+             * Insert/Update pulled into BioTrack.
+             * TODO: Figure out what the 'char userMode' means.
+             */
+            destinationConnection.insertUpdate(tableName, sourceData, yes);
         }
 
         public override bool IsRepeatable()
