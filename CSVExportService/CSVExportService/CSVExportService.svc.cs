fc0505e281113a8125608b1ff54e5e8457d5c011
Aaron <aschryver-cc@conestogac.on.ca>
2016-03-23 15:56:04 -0400

    Created CSVExportService SOAP service

---
+++ CSVExportService/CSVExportService/CSVExportService.svc.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CSVExportService
+{
+    public class CSVExportService : ICSVExportService
+    {
+        #region Globals
+        EventLog eventLog; //Event Logger
+        #endregion
+
+        #region utility
+        /*
+         * Method:  initEventLog()
+         * 
+         * Parameters: Void 
+         * 
+         * Returns: Void
+         * 
+         * Description: Initializes the application for logging events.
+         */
+        private void initEventLog()
+        {
+            string source = "qbExportService";
+
+            try
+            {
+                /*
+                 * Create an event log source if one does not exist...
+                 */
+                if (!EventLog.SourceExists(source))
+                {
+                    EventLog.CreateEventSource(source, "Application");
+                }
+                else
+                {
+                    eventLog = new EventLog();
+                    eventLog.Source = source;
+                }
+            }
+            catch (Exception ex)
+            {
+                /*
+                 * Do something with this exception...
+                 */
+            }
+
+            return;
+        }
+
+        /*
+         * Method:  logEvent()
+         * 
+         * Parameters: string logText
+         * 
+         * Returns: Void
+         * 
+         * Description: Log the event in the Windows Events Logs.
+         */
+        private void logEvent(string logText)
+        {
+            try
+            {
+                eventLog.WriteEntry(logText);
+            }
+            catch (Exception ex)
+            {
+                /*
+                 * Do something with this exception...
+                 */
+            }
+
+            return;
+        }
+        #endregion
+
+        #region Methods
+        public string[] authenticate(string userName, string password)
+        {
+            string[] resultValue = new string[2];
+           string eventText = "";
+
+            /*
+             * Build the event log. 
+             */
+            eventText += "WebMethod        : authenticate()\r\n\r\n";
+            eventText += "Parameters       :\r\n";
+            eventText += "string userName = " + userName + "\r\n";
+            eventText += "string password = " + password + "\r\n"; //Also DON'T do this...
+            eventText += "\r\n"; 
+
+            resultValue[0] = Guid.NewGuid().ToString();
+
+            //TODO: Add real world authentication for validating username and password
+            //That means DON'T do this...
+            if (userName.Equals("username") &&
+                password.Trim().Equals("thisisbad"))
+            {
+                /*
+                 * Empty string for the second string value asking QBWebConnector 
+                 * to connect to the company file that is currently opened in QB.
+                 */
+                resultValue[1] = "c:\\Program Files\\Intuit\\QuickBooks\\sample_product-based business.qbw"; //This is from the sample file...
+            }
+            else
+            {
+                resultValue[1] = "nvu";
+            }
+
+            eventText += "Return Value       :\r\n";
+            eventText += "string resultValue[0] = " + resultValue[0] + "\r\n";
+            eventText += "string resultValue[1] = " + resultValue[1] + "\r\n";
+            eventText += "\r\n";
+
+            /*
+             * Log the event.
+             */
+            logEvent(eventText);
+
+            return resultValue;
+        }
+        
+        public string CSVExport(string token)
+        {
+            string returnValue = "";
+            //Extract table invoice data here
+            CsvExport<string> csve= new CsvExport<string>();
+            string eventText = "";
+
+            /*
+             * Build the event log. 
+             */
+            eventText += "WebMethod        : authenticate()\r\n\r\n";
+            eventText += "Parameters       :\r\n";
+            eventText += "string token = " + token + "\r\n";
+            eventText += "\r\n";
+
+            try
+            {
+                returnValue = csve.Export();
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            eventText += "Return Value       :\r\n";
+            eventText += "string returnValue = " + returnValue + "\r\n";
+            eventText += "\r\n";
+
+            /*
+             * Log the event.
+             */
+            logEvent(eventText);
+
+            return returnValue;
+        }
+        #region
+    }
+}
---
+++ CSVExportService/CSVExportService/CsvExport.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Text; //String manipulation and formatting
+using System.Reflection; //PropertyInfo class
+
+namespace CSVExportService
+{
+    public class CsvExport<T> where T: class
+    {
+        private List<T> ObjectList; //List of objects to export to csv file friendly.
+
+        /*
+         * Constructor
+         */
+        public CsvExport(List<T> objectList)
+        {
+            ObjectList = objectList; //Set the private list of objects to export.
+        }
+
+        /*
+         * Method:  ConvertToCsv()
+         * Parameters:  obj - value to be converted to Csv friendly format.
+         * Description: Converts a object value to a Csv friendly formatted string and returns it.
+         */
+        private string ConvertToCsv(object obj)
+        {
+            string value = obj.ToString();
+
+            if (obj == null)
+            {
+                value = "";
+            }
+            else if (obj is DateTime)
+            {
+                if(((DateTime)obj).TimeOfDay.TotalSeconds == 0)
+                {
+                    value = ((DateTime)obj).ToString("yyyy-MM-dd");
+                }
+                else
+                {
+                    value = ((DateTime)obj).ToString("yyyy-MM-dd HH:mm:ss");
+                }                
+            }
+            else
+            {
+                if (value.Contains(",") ||
+                    value.Contains("\""))
+                {
+                    value = '"' +
+                            value.Replace("\"", "\"\"") +
+                            '"';
+                }
+            }
+
+            return value;
+        }
+
+        /*
+         * Method:      Export()
+         * Parameters:  header - Include the name of the property information of the object.
+         * Description: Returns a .csv file friendly string. Headers are on by default; pass false to exclude them.
+         */ 
+        public string Export(bool header = true)
+        {
+            StringBuilder sb = new StringBuilder(); //Csv friendly string to build.
+
+            IList<PropertyInfo> objectInfo = typeof(T).GetProperties(); //The property information list of the objects.
+
+            /*
+             * Enter if header information is to be included.
+             */ 
+            if(header)
+            {
+                /*
+                 * For each object information in the object information list, append the header information to the string.
+                 */ 
+                foreach(PropertyInfo info in objectInfo)
+                {
+                    sb.Append(info.Name).Append(",");
+                }
+
+                sb.Remove(sb.Length - 1, 1).AppendLine();
+            }
+
+            /*
+             * For each object in the object list, get the values of its properties and append it to the string.
+             */ 
+            foreach(T obj in ObjectList)
+            {
+                foreach(PropertyInfo info in objectInfo)
+                {
+                    /*
+                     * Append value of the objects property here. Make sure it is in friendly to .csv formatting...
+                     * This means calling a seperate method and returning the result here...
+                     * 
+                     * The prototype could be something like this:
+                     * 
+                     * string methodName(object val);
+                     */
+                    sb.Append(ConvertToCsv(info.GetValue(obj))).Append(",");
+                }
+
+                sb.Remove(sb.Length - 1, 1).AppendLine();
+            }
+
+            return sb.ToString();
+        }
+    }
+}
